972243f1fab02ab4c31b8c4600f43749bc76a9e0
D.venkateswarlu <D.venkateswarlu@LAPTOP-9SO84KHE>
2022-04-14 15:49:11 +0530

    welcome to blood bank

---
+++ src/bloodbank/bbb.java
@@ -0,0 +1,10 @@
+package bloodbank;
+
+public class bbb {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to blood bank project");
+		System.out.println("please donate blood");
+	}
+
+}
